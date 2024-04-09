4fda684f92a163a3a8a9181f395204f090100db6
mathildeolesen <111729553+mathildeolesen@users.noreply.github.com>
2024-04-09 12:02:26 +0200

    Services for henting av data fra Sanity
    (Forelesning 22 - 4.april)
    
    * Oppdatert komponentene til å nå ta i bruk Sanity-data
    - ContentPage.jsx gjør get() kall, og sender videre
    
    * Services tar i bruk sanity sin createClient() (client her, vi lagde den forrige forelesning)
    
    * async fetch() i services og get() i componentene
    - fetch() jobber i bakgrunnen og get() henter når den trenger.
    
    * GROQ i fetch() for å gjøre spørring
    - *[_type == "products"] osv

--- frontend/sanity/services/productServices.js
+++ frontend/sanity/services/productServices.js
@@ -1,6 +1,14 @@
 import { client } from "../client";
 
 export async function fetchAllProduct() {
-    const data = await client.fetch('*[_type == "products"]')
+    const data = await client.fetch(`*[_type == "products"] {
+        _id,
+        productname,
+        price,
+        stock,
+        "catname": category->categorytitle,
+        "catslug": category->categoryurl.current,
+        "image": productimage.asset->url
+    }`)
     return data
 }
