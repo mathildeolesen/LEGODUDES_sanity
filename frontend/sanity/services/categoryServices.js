7e0a007e8d3cbc4b3e8a90d3cc34fc015b7102d6
mathildeolesen <111729553+mathildeolesen@users.noreply.github.com>
2024-04-10 23:57:31 +0200

    Join-spørring (^) for å ta med produkter tilhørende kategori
    (Forelesning 23 - 9.april)
    
    * ^ —> peker til objektet som blir returnert av forrige spørring
    - I dette tilfellet er forrige spørring:
    - *[_type == "categories" && categoryurl.current == $slug]
    - som returnerer et kategori-"objekt"
    - ^ gir oss da: kategori._id

--- frontend/sanity/services/categoryServices.js
+++ frontend/sanity/services/categoryServices.js
@@ -10,6 +10,25 @@ export async function fetchAllCategories() {
 }
 
 export async function fetchCategoryBySlug(slug) {
-    const data = await client.fetch(`*[_type == "categories" && categoryurl.current == $slug]`, {slug})
+    const data = await client.fetch(`*[_type == "categories" && categoryurl.current == $slug] {
+        _id,
+        categorytitle,
+        "catProducts": *[_type == "products" && references(^._id)] {
+            _id,
+            productname,
+            "slug": producturl.current,
+            price,
+            stock,
+            "catname": category->categorytitle,
+            "catslug": category->categoryurl.current,
+            "image": productimage.asset->url
+        }
+    }`, {slug})
     return data
 }
+
+/*
+    Prøver å lage GROQ som kan hente produkter som tilhører en kategori
+    category._ref tilhører produkter og _id tilhører kategorier (kategoriID = _id) 
+    // *[_type == "products" && category._ref == _id]
+*/
